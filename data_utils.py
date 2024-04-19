7549b3fba966ad85221d02b42f2d31fabaf8d779
Limeng Pu <pulimeng0722@gmail.com>
2024-04-19 12:56:38 -0500

    Add files via upload

--- data_utils.py
+++ data_utils.py
@@ -8,13 +8,13 @@ def LoadData(path):
     with h5py.File(path, 'r') as f:
         X = f['X'][()]
         eI = f['eI'][()]
-        eAttr = f['eAttr'][()]
+        # eAttr = f['eAttr'][()]
         y = f['y'][()]
     X = torch.tensor(X, dtype=torch.float32)
     eI = torch.tensor(eI, dtype=torch.long)
-    eAttr = torch.tensor(eAttr, dtype=torch.float32)
+    # eAttr = torch.tensor(eAttr, dtype=torch.float32)
     y = torch.tensor(y, dtype=torch.long)
-    data = Data(x=X, edge_index=eI, edge_attr=eAttr, y=y)
+    data = Data(x=X, edge_index=eI, y=y)
     return data
 
 class MyGraphDataset(Dataset):
@@ -28,6 +28,8 @@ class MyGraphDataset(Dataset):
         return len(self.df)
     
     def __getitem__(self, idx):
-        filename = self.df.loc[idx, 'filename'] # TODO: change this line accordingly
-        data = torch.load(osp.join(self.root, filename))
+        filename = self.df.loc[idx, 'instance']  # Assuming 'instance' is the column containing filenames
+        data = LoadData(osp.join(self.root, filename, filename + '.h5'))
+        # Convert data to device if needed
+        data = data.to(self.device)
         return data
